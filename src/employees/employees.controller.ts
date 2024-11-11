4b910b97aa970b7148aea2985417d4e9bd84891e
jurayevdev <jurayevcoder@gmail.com>
2024-11-11 10:18:35 +0500

    change employee upade and remove

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -18,14 +18,14 @@ import { RolesGuard } from 'src/common/guards/roles.guard';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @ApiTags('Employee')
-// @ApiBearerAuth('access-token')
+@ApiBearerAuth('access-token')
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @ApiOperation({ summary: 'Create a new employee' })
-  // @Roles('owner', 'manager')
-  // @UseGuards(RolesGuard, JwtAuthGuard)
+  @Roles('owner', 'manager')
+  @UseGuards(RolesGuard, JwtAuthGuard)
   @Post(':clinic_id')
   create(
     @Param('clinic_id') clinic_id: string,
@@ -35,30 +35,30 @@ export class EmployeesController {
   }
 
   @ApiOperation({ summary: 'View all employees' })
-  // @Roles('owner', 'manager')
-  // @UseGuards(RolesGuard, JwtAuthGuard)
+  @Roles('owner', 'manager')
+  @UseGuards(RolesGuard, JwtAuthGuard)
   @Get(':clinic_id')
   findAll(@Param('clinic_id') clinic_id: string) {
     return this.employeesService.findAll(clinic_id);
   }
 
   @ApiOperation({ summary: 'Paginate employees' })
-  // @Roles('owner', 'manager')
-  // @UseGuards(RolesGuard, JwtAuthGuard)
+  @Roles('owner', 'manager')
+  @UseGuards(RolesGuard, JwtAuthGuard)
   @Get(':clinic_id/page')
   paginate(@Param('clinic_id') clinic_id: string, @Query('page') page: number) {
     return this.employeesService.paginate(clinic_id, page);
   }
 
   @ApiOperation({ summary: 'View a employee by ID' })
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get(':clinic_id/:id')
   findOne(@Param('clinic_id') clinic_id: string, @Param('id') id: string) {
     return this.employeesService.findOne(clinic_id, id);
   }
 
   @ApiOperation({ summary: 'Update a employee by ID' })
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Put(':clinic_id/:id')
   update(
     @Param('clinic_id') clinic_id: string,
@@ -69,8 +69,8 @@ export class EmployeesController {
   }
 
   @ApiOperation({ summary: 'Delete a employee by ID' })
-  // @Roles('owner', 'manager')
-  // @UseGuards(RolesGuard, JwtAuthGuard)
+  @Roles('owner', 'manager')
+  @UseGuards(RolesGuard, JwtAuthGuard)
   @Delete(':clinic_id/:id')
   remove(@Param('clinic_id') clinic_id: string, @Param('id') id: string) {
     return this.employeesService.remove(clinic_id, id);
